ec8d2e903581c1ec209afc66c54e6a2ca7384343
Nwadike Joseph <jcyoung.mjc44@gmail.com>
2023-07-26 11:11:25 +0100

    fix task 4 trailing whitespace

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -4,7 +4,7 @@ caching system:
 
 You must use self.cache_data - dictionary from the parent class
 BaseCaching
-You can overload def __init__(self): but don’t forget to call the 
+You can overload def __init__(self): but don’t forget to call the
 parent init: super().__init__()
 def put(self, key, item):
     Must assign to the dictionary self.cache_data the item value
